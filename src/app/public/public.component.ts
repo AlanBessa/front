4101f0f8ac45229e50e38634134b13539cc6d9bf
André Noedis Fossaluza <andrefossaluza@hotmail.com>
2017-07-05 14:45:36 -0300

    inicialização do serviço de login e cadastro para o componente public

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { LoginService } from '@app/account/login/login.service';
 
 @Component({
     template:  `<router-outlet></router-outlet>`
 })
 export class PublicComponent {
+    public constructor(
+        private _loginService: LoginService
+    ) {
+    }
+
+    ngOnInit(): void {
+        this._loginService.init();
+    }
 }

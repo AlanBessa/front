a18bbb0e4338f7f872061d1582399e74ed001224
DESKTOP-79JMJNQ\Alan Bessa <bessa.alan@gmail.com>
2017-07-11 15:02:54 -0300

    Ajuste da localização de data e erros de gramatica

--- src/app/public/post-a-task/post-a-task.component.ts
+++ src/app/public/post-a-task/post-a-task.component.ts
@@ -9,6 +9,7 @@ import { AppConsts } from '@shared/AppConsts';
 import { MapsAPILoader } from '@agm/core';
 import * as _ from 'lodash';
 import * as moment from "moment"; 
+import 'moment/min/locales';
 import { LoginService } from "app/account/login/login.service";
 
 declare var google: any;
@@ -85,6 +86,7 @@ export class PostTaskComponent extends AppComponentBase implements AfterViewInit
     }
 
     ngOnInit(): void {
+        moment.locale('pt-br');
         this.administrativeAreas.items = this.administrativeAreas.items.filter(x => x.id == "RJ");
         if (abp.session.userId) 
                 this.public = false;
@@ -204,7 +206,7 @@ export class PostTaskComponent extends AppComponentBase implements AfterViewInit
         currentDate.setHours(0,0,0,0)
         //console.log(event);
         if(date < currentDate){
-            this.message.error("Selecione um data igual ou posterior a data atual.", "Data inválida!")
+            this.message.error("Selecione um data igual ou posterior à data atual.", "Data inválida!")
         }else{
             this.scheduleDateDisplay = moment(date).format('L');
             this.showDataPicker = false;

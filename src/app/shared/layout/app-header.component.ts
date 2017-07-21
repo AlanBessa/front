b3ab65b200fa89cf7d2404882c962936555789cd
DESKTOP-79JMJNQ\Alan Bessa <bessa.alan@gmail.com>
2017-07-21 18:29:13 -0300

    Menu logado

--- src/app/shared/layout/app-header.component.ts
+++ src/app/shared/layout/app-header.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit, Injector, ViewEncapsulation, ViewChild } from '@angular/core';
+﻿import { Component, OnInit, Injector, ViewEncapsulation, ViewChild, Input } from '@angular/core';
 import { AbpSessionService } from '@abp/session/abp-session.service';
 import { AppSessionService } from '@shared/common/session/app-session.service';
 import { AbpMultiTenancyService } from '@abp/multi-tenancy/abp-multi-tenancy.service';
@@ -8,7 +8,9 @@ import {
     UserServiceProxy,
     LinkedUserDto,
     TenantLoginInfoDto,
-    GetCurrentLoginInformationsOutput
+    GetCurrentLoginInformationsOutput,
+    EntityDtoOfInt64,
+    WorbbiorServiceProxy
 } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { AppAuthService } from '@app/shared/common/auth/app-auth.service';
@@ -18,6 +20,10 @@ import { Router, ActivatedRoute } from '@angular/router';
 
 import * as _ from 'lodash';
 import * as moment from 'moment';
+import { ChangeProfilePictureModalComponent } from "app/shared/layout/profile/change-profile-picture-modal.component";
+import { UserMenu } from "app/shared/layout/user-menu";
+import { UserMenuItem } from "app/shared/layout/user-menu-item";
+import { UtilsService } from "abp-ng2-module/src/utils/utils.service";
 
 @Component({
     templateUrl: './app-header.component.html',
@@ -26,16 +32,22 @@ import * as moment from 'moment';
 })
 export class AppHeaderComponent extends AppComponentBase implements OnInit {
 
+    @Input('changePasswordModal') changePasswordModal: ChangeProfilePictureModalComponent;
+
+    profilePicture: string = "/assets/common/images/default-profile-picture.png";
+    currentRoleName: string = "";
+    switchRole: boolean = false;
+    toWorbbiorRegister: boolean = false;
+    userName: string;
+
     languages: abp.localization.ILanguageInfo[];
     currentLanguage: abp.localization.ILanguageInfo;
     isImpersonatedLogin: boolean = false;
-    currentRoleName:string = "";
 
     isAuthenticated:boolean = false;
 
     shownLoginNameTitle: string = "";
     shownLoginName: string = "";
-    profilePicture: string = "/assets/common/images/default-profile-picture.png";
     recentlyLinkedUsers: LinkedUserDto[];
     unreadChatMessageCount = 0;
 
@@ -43,6 +55,7 @@ export class AppHeaderComponent extends AppComponentBase implements OnInit {
 
     public isWorbbior: boolean = false;
     public isMobile: boolean = false;
+    public menuOpen:string = "";
 
     constructor(
         injector: Injector,
@@ -53,7 +66,9 @@ export class AppHeaderComponent extends AppComponentBase implements OnInit {
         private userLinkServiceProxy: UserLinkServiceProxy,
         private userServiceProxy: UserServiceProxy,
         private router: Router,
-        private _authService: AppAuthService
+        private _authService: AppAuthService,
+        private _worbbiorService: WorbbiorServiceProxy,
+        private _utilsService: UtilsService
     ) {
         super(injector);
     }
@@ -74,6 +89,10 @@ export class AppHeaderComponent extends AppComponentBase implements OnInit {
 
         this.isWorbbior = this.appSession.userRoleName == "Worbbior";
         this.isMobile = window.screen.width < 768;
+
+        this.userName = this.appSessionService.user.name;
+        this.switchRole = this.permission.isGranted("Pages.Worbbior.SwitchToWorbbientProfile");
+        this.toWorbbiorRegister = !this.switchRole ? true : false ;
     }
 
     changeLanguage(languageName: string): void {
@@ -135,5 +154,82 @@ export class AppHeaderComponent extends AppComponentBase implements OnInit {
              this.appSessionService.userRoleName = "Worbbient";
             this.router.navigate(['/post-a-task']);
         }
+    }
+
+    showHideMenu(): void {
+        if (this.menuOpen == "") {
+            this.menuOpen = "open";
+        } else {
+            this.menuOpen = "";
+        }
+    }
+
+    menu: UserMenu = new UserMenu("UserMenu", "UserMenu", [
+        new UserMenuItem("MyWorbby", "Pages.Worbbient.MyWorbby", "Worbbient", "", "/worbbient/my-worbby"),
+        new UserMenuItem("TasksHistory", "Pages.Worbbient.TasksHistory", "Worbbient", "", "/worbbient/tasks-history"),
+        new UserMenuItem("PaymentsHistory", "Pages.Worbbient.PaymentsHistory", "Worbbient", "", "/worbbient/payments-history"),
+        new UserMenuItem("EditProfile", "Pages.Worbbient.EditProfile", "Worbbient", "", "/worbbient/edit-profile"),
+
+        new UserMenuItem("MyWorbby", "Pages.Worbbior.MyWorbby", "Worbbior", "", "/worbbior/my-worbby"),
+        new UserMenuItem("TasksHistory", "Pages.Worbbior.TasksHistory", "Worbbior", "", "/worbbior/tasks-history"),
+        new UserMenuItem("PaymentsHistory", "Pages.Worbbior.PaymentsHistory", "Worbbior", "", "/worbbior/payments-history"),
+        new UserMenuItem("EditProfile", "Pages.Worbbior.EditProfile", "Worbbior", "", "/worbbior/edit-profile"),
+        new UserMenuItem("ViewProfile", "Pages.Worbbior.EditProfile", "Worbbior", "", "/worbbior/page/", this.appSessionService.worbbiorSlug ? this.appSessionService.worbbiorSlug : ""),
+    ]);
+    
+    showMenuItem(menuItem): boolean {
+        if (menuItem.permissionName) {
+            return this.permission.isGranted(menuItem.permissionName) && menuItem.roleName == this.currentRoleName;
+        }
+
+        return true; 
+    }
+ 
+    switchToWorbbiorProfile(){
+        this.appSessionService.userRoleName = "Worbbior";
+        setTimeout(
+            function(){ 
+                location.href = "/";
+            },
+        500);        
+    }
+
+    switchToWorbbientProfile() {
+        this.appSessionService.userRoleName = "Worbbient";
+        setTimeout(
+            function(){ 
+                location.href = "/";
+            },
+        500);
+    }
+
+    worbbientToWorbbior():void {
+        this.message.confirm(
+            "Deseja tornar-se também um Worbbior?", "Seja um Worbbior!",
+            isConfirmed => {
+                if (isConfirmed) {
+                    
+                    this._worbbiorService.worbbientToWorbbior(new EntityDtoOfInt64({ id: abp.session.userId }))
+                    .finally(() => {
+                    })
+                    .subscribe(() => {
+                        this.message.custom('Precisamos de mais algumas informações sobre você, preencha os dados na próxima página para ganhar dinheiro oferecendo suas habilidades. Mas atenção, só após completar todos os campos, o seu perfil Worbbior será ativado na plataforma.', 'Falta pouco para você ser um worbbior!', 'assets/common/images/default-profile-picture.png').done(() => {
+                            this.appSessionService.userRoleName = "Worbbior";
+                            location.href = "/";
+                        });
+                    });;
+                }
+            }
+        );        
+    }
+
+    logout(): void {
+        this._utilsService.deleteCookie("userRoleName");
+        this._utilsService.deleteCookie("firstLoginUser");
+        this._authService.logout();        
+    }
+
+    changePassword():void{
+        abp.event.trigger("changePasswordModal");
     }
 }
